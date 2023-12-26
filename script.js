fdeb36aec12ffccd598005992d22984d3cf44d2b
Mukulchauhan1 <97191692+Mukulchauhan1@users.noreply.github.com>
2023-12-26 17:56:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+ 
+
+ function bulbOn(){
+    let bulbobj= document.querySelector(".bulboff");
+    bulbobj.setAttribute("src", "bulb-on.png");
+ }
+
+ function bulbOff(){
+    let bulbobj= document.querySelector(".bulboff");
+    bulbobj.setAttribute("src", "bulb-off.png");
+ }
